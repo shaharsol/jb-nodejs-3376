fd1421f565a33e2e533fabeaf161d5fe140f4718
galamo <galamouyal88@gmail.com>
2024-05-30 12:00:47 +0300

    removed some lines

--- cryptoapp/src/app.ts
+++ cryptoapp/src/app.ts
@@ -5,7 +5,6 @@ import path from 'path';
 import notFound from './middlewares/error/404';
 import error from './middlewares/error/error';
 
-
 // app config
 const app = express();
 app.set('view engine', 'ejs');
@@ -14,12 +13,9 @@ app.set('views', path.resolve(__dirname, 'views'))
 // routing
 app.use('/users', usersRouter)
 
-
 // errors
 app.use(notFound);
 app.use(error);
-
-
 
 app.listen(config.get('app.port'), () => {
     console.log(`${config.get('app.name')} with secret ${config.get('app.secret')} started on ${config.get('app.port')}`)
