98b488a0d4c23ab0dd1078d4441dd530e95d31e2
galamo <galamouyal88@gmail.com>
2024-05-30 10:15:17 +0300

    config

--- cryptoapp/src/app.ts
+++ cryptoapp/src/app.ts
@@ -1,1 +1,9 @@
-console.log('hello world')
+import config from 'config';
+import express from 'express';
+
+
+const app = express();
+
+app.listen(config.get('app.port'), () => {
+    console.log(`${config.get('app.name')} with secret ${config.get('app.secret')} started on ${config.get('app.port')}`)
+})
