d7a63e47712dee7e787973a0111767ce992f7375
galamo <galamouyal88@gmail.com>
2024-06-03 09:12:20 +0300

    auth

--- cryptoapp/src/middlewares/enforce-auth.ts
+++ cryptoapp/src/middlewares/enforce-auth.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function (req: Request, res: Response, next: NextFunction) {
-    if (!req.user?.id) return res.redirect('/welcome')
+    if (!(req.user?.id)) return res.redirect('/welcome')
     next();
 }
--- cryptoapp/src/middlewares/enforce-guest.ts
+++ cryptoapp/src/middlewares/enforce-guest.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function (req: Request, res: Response, next: NextFunction) {
-    if (req.user?.id) return res.redirect('/users/dashboard')
+    // if (req.user) return res.redirect('/users/dashboard')
     next();
 }
