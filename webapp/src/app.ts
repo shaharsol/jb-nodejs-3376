d87cbb006c591d1ff9b3af8026ac4ca27cbbb0ec
shaharsol <shahar@gnift.com>
2024-06-09 14:52:56 +0300

    config works

--- webapp/src/app.ts
+++ webapp/src/app.ts
@@ -4,5 +4,5 @@ import express from "express";
 const app = express();
 
 app.listen(config.get('app.port'), () => {
-    console.log(`app started on http://localhost:${config.get('app.port')}`)
+    console.log(`${config.get('app.name')} with awsToken ${config.get('app.awsToken')} started on http://localhost:${config.get('app.port')}`)
 })
