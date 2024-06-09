0953d01a67d2c0c4bd2a3b06c516c1e879e0c5ef
shaharsol <shahar@gnift.com>
2024-06-09 15:27:56 +0300

    form shows

--- webapp/src/app.ts
+++ webapp/src/app.ts
@@ -1,13 +1,20 @@
 import config from "config";
 import express from "express";
 import usersRouter from './routers/users'
+import path from "path";
 
 const app = express();
 
+// app setting
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'))
 
+// middlewares
 
+// routing
 app.use('/users', usersRouter);
 
+// starting
 app.listen(config.get('app.port'), () => {
     console.log(`${config.get('app.name')} with awsToken ${config.get('app.awsToken')} started on http://localhost:${config.get('app.port')}`)
 })
