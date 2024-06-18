e8d7dad4a67aa40b147ba0d3122e82a42e7c24dc
shaharsol <shahar@gnift.com>
2024-06-18 14:13:15 +0300

    mongo

---
+++ webapp/src/worker.ts
@@ -0,0 +1,11 @@
+import { getModel } from "./models/symbol-value/factory";
+
+(async() => {
+    const symbolValue = {
+        symbol: 'BTC',
+        value: 60000,
+        when: new Date()
+    }
+    const result = await getModel().add(symbolValue)
+    console.log(result)
+})
