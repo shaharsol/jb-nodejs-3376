892271c81b9ac6247ffc470abb7c65399f405c4b
shaharsol <shahar@gnift.com>
2024-06-18 11:38:13 +0300

    create table users

--- webapp/src/initdb.ts
+++ webapp/src/initdb.ts
@@ -5,11 +5,21 @@ import { promisify } from 'util';
 const connection = mysql.createConnection(config.get('mysql'));
 
 const connect = promisify(connection.connect).bind(connection);
+const query = promisify(connection.query).bind(connection);
 
 (async() => {
     try {
         await connect();
         console.log('connected')
+
+        await query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id int auto_increment,
+                github_id varchar(255) not null,
+                primary key (id)
+            ) 
+        `)
+        console.log('created users table')
     
     } catch (err) {
         console.error(err)
