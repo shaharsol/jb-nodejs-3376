0abc3d6f6e73ef7e71587c7f2e9d9b5149923abb
shaharsol <shahar@gnift.com>
2024-09-24 10:05:44 +0300

    basic dashboard

--- migdal2/src/app.ts
+++ migdal2/src/app.ts
@@ -1,8 +1,16 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import config from 'config'
+import path from 'path'
 
 const port = config.get<number>('app.port')
 const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'views'))
+
+app.get('/users/dashboard', (req: Request, res: Response, next) => {
+    res.render('users/dashboard')
+})
 
 app.listen(port, () => {
     console.log(`app started on http://localhost:${port}`)
