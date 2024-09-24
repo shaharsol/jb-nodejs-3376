92f03e9c361f43e0b330075045cf3cdd5541dc51
shaharsol <shahar@gnift.com>
2024-09-24 14:26:18 +0300

    mysql

---
+++ migdal2/src/db/mysql.ts
@@ -0,0 +1,9 @@
+import mysql from 'mysql2'
+import config from 'config'
+import { promisify } from 'util'
+
+const pool = mysql.createPool(config.get('mysql'))
+
+const query = promisify(pool.query).bind(pool);
+
+export default query;
