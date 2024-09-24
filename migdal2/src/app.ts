334c8024144a7b18310afef8aec71ee7d3d7b72c
shaharsol <shahar@gnift.com>
2024-09-24 09:52:10 +0300

    config

--- migdal2/src/app.ts
+++ migdal2/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import config from 'config'
 
+const port = config.get<number>('app.port')
 const app = express()
 
-app.listen(8080, () => {
-    console.log('app started...')
+app.listen(port, () => {
+    console.log(`app started on http://localhost:${port}`)
 })
 
 
